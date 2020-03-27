08ce0e51ccbc235bd073d398bc3041b5ca9d7d16
David Yallup <dyallup@cern.ch>
2020-03-27 17:51:00 +0100

    add utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+import subprocess
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print output.strip()
+    rc = process.poll()
+    return rc

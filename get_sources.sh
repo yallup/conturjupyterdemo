b6d273f80e9835484a5731d414a462ac2c76f0f3
David Yallup <dyallup@cern.ch>
2020-03-27 17:56:12 +0100

    fix where things are

--- get_sources.sh
+++ get_sources.sh
@@ -4,5 +4,3 @@ cp -r $CONTURMODULEDIR/AnalysisTools/GridSetup/param_file.dat Exercises/
 cp -r $CONTURMODULEDIR/Models/DM/DM_vector_mediator_HF_UFO Exercises/GridPack/DM
 
 cp -r $CONTURMODULEDIR/AnalysisTools/contur/tests/sources/myscan00 Exercises/exampleGrid
-
-export PYTHONPATH=$PYTHONPATH:$PWD
